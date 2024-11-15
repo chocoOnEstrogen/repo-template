5174ebe2f7570a3e1356eaaff9d3662ec79d40cf
Stella (Puppy Girl) <hcox.dev@gmail.com>
2024-11-15 12:53:06 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// A simple function to greet the user
+function greet(name: string): string {
+	return `Hello, ${name}!`
+}
+
+// Example usage
+const userName = 'World'
+console.log(greet(userName))
